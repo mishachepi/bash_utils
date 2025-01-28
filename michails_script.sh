e3e760a4a6ca1e55c0b9eb5ef76202b0e2ba25b7
Pavlo Panukhnyk <panukhnykpa@gmail.com>
2025-01-28 10:15:03 +0100

    modified

--- michails_script.sh
+++ michails_script.sh
@@ -7,7 +7,6 @@ if [ "$DF_ROOT" -gt "$TARGET" ]; then
         echo "Места на диске не достаточно"
         echo "Доступно меньше $TARGET %"
         du -ah / | sort -rh | head -n 10 
-
 else
         echo "Места на диске достаточно"
 fi
